914ef606c6a30bc779087240d202e639af370747
Acortes1403 <ajclrakirute@gmail.com>
2024-11-10 17:50:38 -0600

    SecurePath 2.5

--- SecurePath-main/app/src/main/java/com/example/avance/model/FormModel.kt
+++ SecurePath-main/app/src/main/java/com/example/avance/model/FormModel.kt
@@ -1,17 +1,20 @@
 package com.example.avance.model
 
 data class FormData(
+    //Formulario general
     var name: String = "",
     var date: String = "",
     var location: String = "",
     var time: String = "",
     var transectNumber: String = "",
+    //Formulario 1 - 2 - 3
     var selectedAnimal: String? = null,
     var selectedObservation: String = "La Vió",
     var commonName: String = "",
     var scientificName: String = "",
     var individualCount: String = "",
     var observationNotes: String = "",
+    //Formulario 4
     var selectedRegistro: String = "",
     val selectedGrowthHabit: String? = null,
     val placa: String = "",
@@ -19,6 +22,7 @@ data class FormData(
     val distance: String = "",
     val biomonHeight: String = "",
     val height: String = "",
+    //Formulario 5
     val selectedSubQuadrant: String = "",
     val zone: String = "",
     val cameraName: String = "",
@@ -28,12 +32,14 @@ data class FormData(
     val installationDate: String = "",
     val targetDistance: String = "",
     val lensHeight: String = "",
+    //Formulario 6
     val isProgrammed: Boolean = false,
     val hasMemory: Boolean = false,
     val hasGateTest: Boolean = false,
     val isInstalled: Boolean = false,
     val hasCameraSign: Boolean = false,
     val isOn: Boolean = false,
+    //Formulario 7
     val rainfall: String = "",
     val maxTemperature: String = "",
     val minTemperature: String = "",
