20f5fa0381b474172da4066b1ea227600bd27c36
Acortes1403 <ajclrakirute@gmail.com>
2024-11-19 14:57:24 -0600

    Secure Path 2.56 - Amhed

--- SecurePath-main/app/src/main/java/com/example/avance/view/tiposformularios/FormSelect3.kt
+++ SecurePath-main/app/src/main/java/com/example/avance/view/tiposformularios/FormSelect3.kt
@@ -43,7 +43,7 @@ fun FormSelect3(
                 .verticalScroll(rememberScrollState())
         ) {
             // Campo de Código
-            FormTextField("Código", formData.commonName, fontSize) { viewModel.updateCodigof4(it) }
+            FormTextField("Código", formData.codigof4, fontSize) { viewModel.updateCodigof4(it) }
 
             Spacer(modifier = Modifier.height(16.dp))
 
